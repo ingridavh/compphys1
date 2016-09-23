902b71e11737712c928338d3aa18481d20e02a0e
ingridavh <ingridavh@hotmail.com>
2016-09-23 11:12:52 +0200

    added project 2 folder

--- FYS4150_project1/my_lu.h
+++
@@ -1,7 +0,0 @@
-#ifndef LU
-#define LU
-
-double my_lu( int N=10 , bool finderr = false, bool CPU = false, bool fileprint = false, bool CPU_avg = false);
-
-#endif // LU
-
--- FYS4150_project1/specialized.h
+++
@@ -1,6 +0,0 @@
-#ifndef SPECIALIZED
-#define SPECIALIZED
-double specialized (int N=10, bool finderr = false, bool CPU = false, bool fileprint = false, bool CPU_avg = false);
-
-#endif // SPECIALIZED
-
